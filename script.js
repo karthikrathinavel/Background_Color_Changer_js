9f468adad7b7c129fb71ffb589ad54e7d19a3314
Karthik Rathinavel <80736284+karthikrathinavel@users.noreply.github.com>
2021-05-25 09:41:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple','black','orange'];
+
+body.style.backgroundColor = 'violet';
+button.addEventListener('click', changeBackground);
+
+function changeBackground(){
+const colorIndex= parseInt(Math.random()*colors.length);
+body.style.backgroundColor = colors[colorIndex];
+}
